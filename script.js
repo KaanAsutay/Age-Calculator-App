6f4e76dd3d4e35d6b9a8491fb341b7d0889f80e7
Kaan <kaanasutay10@gmail.com>
2024-04-22 23:03:23 +0300

    create let result

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let userInput = document.getElementById("date")
 userInput.max = new Date().toISOString().split("T")[0]
 
+let result = document.getElementById("result")
+
 function calculateAge(){
     let birthDate = new Date(userInput.value);
 
