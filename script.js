33f0035d98ebdf8c3e2c68143b329dd1ad160609
Kaan <kaanasutay10@gmail.com>
2024-04-22 23:12:21 +0300

    FINISH Age Calculator App

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ function calculateAge(){
 
     let today = new Date();
 
-    let d2 = birthDate.getDate();
-    let m2 = birthDate.getMonth() + 1;
-    let y2 = birthDate.getFullYear();
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
 
     let d3, m3, y3;
 
@@ -38,7 +38,8 @@ function calculateAge(){
         m3 = 11
         y3--
     }
-    console.log(y3,m3,d3)
+    // console.log(y3,m3,d3)
+    result.innerHTML = `You are <span> ${y3} </span> years, <span> ${m3} </span> months and <span> ${d3} </span> days old`;
 }
 
 function getDaysInMonth(year, month) {
